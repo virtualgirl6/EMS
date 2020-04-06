766c5723d4ba4dbaca9b21266f4f5e5508862bb5
iehkaatee <ykteerlink28@gmail.com>
2020-04-06 17:30:32 +0200

    cleaner code + new graph

--- Assignement2B_VickyBosch_YuriTeerlink.R
+++ Assignement2B_VickyBosch_YuriTeerlink.R
@@ -15,97 +15,130 @@ setwd("/Users/iehkaatee/Documents/UU/AI/jaar\ 3/exp\ method/Rstudio/EMS")
 
 # load data from experiment 
 load("/Users/iehkaatee/Documents/UU/AI/jaar\ 3/exp\ method/Rstudio/EMS/allData.Rdata")
-summary(allData)
-
-colnames(allData)
-length(unique(score28Data$SubjectNr)) #deelnemers:=  score:aantal, 7:59, 14:48, 28:48
-
-unique(allData$SubjectNr)
-unique(score7Data$WordScore)
-
-#data gesplitst per wordscore
-score7Data <- allData[allData$WordScore == 7,]
-score14Data <- allData[allData$WordScore == 14,]
-score28Data <- allData[allData$WordScore == 28,]
-
-###
-###SINGLE TYPE TASK 
-###
-
-tableTypeTask14 <- score14Data[score14Data$partOfExperiment == "practiceTyping",]
-tableTypeTask14
-
-unique(tableTypeTask14$Eventmessage1)
-test <- allData[allData$Eventmessage1 == "scoreGiven",]
-unique(allData$SubjectNr)
-tableTypeTask141 <- tableTypeTask14[tableTypeTask14$Eventmessage1 == "trailStop",]
-
-nrow(tableTypeTask143[tableTypeTask143$Eventmessage1 =="keypress",])
-nrow(tableTypeTask14[tableTypeTask14$Eventmessage1 =="keypress",])
-nrow(tableTypeTask142[tableTypeTask142$Eventmessage1 =="keypress",])
-ikiCor <- 150/nrow(tableTypeTask14)
-ikiCor
-
-ikiCornNcor <- 150/ length(tableTypeTask14$Eventmessage1)
-ikiCornNcor
-
-ikisCorrectOnly <- with(tableTypeTask14,diff(c(0,LocalTime)))
-ikisCorrectOnly
-
-ikisCorrectAndIncorrect <- with(tableTypeTask14,diff(c(0,LocalTime)))
-ikisCorrectAndIncorrect
-
-summary(sort(ikisCorrectOnly)) ;summary(sort(ikisCorrectAndIncorrect))
-
-library(ggplot2)
-allikis <- c(ikisCorrectOnly, ikisCorrectAndIncorrect)
-labels <-  c(rep("onlyCorrectAnalyzed",length(ikisCorrectOnly)),rep("CorrectAndIncorrectAnalyzed",length(ikisCorrectAndIncorrect)))
-frameIkis <- data.frame(allikis,labels)
-iki_plot <- ggplot(frameIkis, aes(x=labels, y=allikis)) +
-  geom_boxplot(stat = "boxplot")
-print(iki_plot)
 
 
-###
-###SCRABBLE TYPE TASK
-###
+
+####
+#### SINGLE TYPE TASK 
+####
+unique(allData$Eventmessage1)
+test <- drop.levels(subset(allData, partOfExperiment == "practiceTyping"  & WordScore == 14 & Eventmessage1 == 'trialStop'))
+tableTypeTask14 <- drop.levels(subset(allData, partOfExperiment == "practiceTyping" & Eventmessage1 == "keypress" & WordScore == 14))
+tableTypeTask28 <- drop.levels(subset(allData, partOfExperiment == "practiceTyping" & Eventmessage1 == "keypress" & WordScore == 28))
+nrow(tableTypeTask28)
+
+# Average InterKeypress Interval (IKI)
+iki14 <- 150/nrow(tableTypeTask14) * length(unique(tableTypeTask14$SubjectNr))
+iki28 <- 150/nrow(tableTypeTask28) * length(unique(tableTypeTask28$SubjectNr))
+
+# Letters per unit(LPU)
+lpu14 <- 6/iki14
+lpu28 <- 6/iki28
+
+# Compute the actual score
+unitTypeTask <- 0:25
+typeTask14 = data.frame(unit = unitTypeTask)
+typeTask14$points <- lpu14 * unitTypeTask
+
+typeTask28 = data.frame(unit = unitTypeTask)
+typeTask28$points <- lpu28 * unitTypeTask
+
+iki_plot <- ggplot(typeTask14, aes(x=unit, y=points))+
+  geom_point(shape=21,colour="black", fill='black', size = 1) + #geom_line() +
+  xlab("Time (unit)") +
+  ylab("Score") +
+  ggtitle("Clear plot") 
+
+viki_plot <- iki_plot + geom_point(data=typeTask28, colour="green",aes(x=unit, y=points));viki_plot
+
+
+####
+#### SCRABBLE TYPE TASK
+####
+
+# create subset of the allData dataframe needed to estimate parameters for the model.
+tableScrabbleTask14 <- drop.levels(subset(allData, partOfExperiment == "practiceScrabble" 
+                                        & allData$WordScore == 14  
+                                        & Eventmessage2 == "correctNewWord"))
+
+tableScrabbleTask28 <- drop.levels(subset(allData, partOfExperiment == "practiceScrabble" 
+                              & allData$WordScore == 28  
+                              & Eventmessage2 == "correctNewWord"))
 
 scrabbleTypeTask <- allData[allData$partOfExperiment == "practiceScrabble" & allData$WordScore == 28 &
                               allData$Eventmessage2 == "correctNewWord",]
 tableScrabbleTask2 <- drop.levels(scrabbleTypeTask)
 
-#summary(tableScrabbleTask2[tableScrabbleTask2$Eventmessage1 == "scoreGiven",])
+# scale Localtime to units 
+tableScrabbleTask14$unit <- tableScrabbleTask14$LocalTime / 6
+tableScrabbleTask28$unit <- tableScrabbleTask28$LocalTime / 6
 
-#unique(tableScrabbleTask2[tableScrabbleTask2$Eventmessage2 == "correctNewWord",]$currentScrabbleLettersTyped)
-
-tableScrabbleTask3 <- tableScrabbleTask2[tableScrabbleTask2$LocalTime <= 150,]
-tableScrabbleTask3$unit <- tableScrabbleTask3$LocalTime / 6
-unique(tableScrabbleTask2$scrabbleCondition)
-
-## Clear and simple plot in which I have influenced what values are shown on the x- and y-axes
-plot3 <- ggplot(tableScrabbleTask3, aes(x=unit, y=CurrentScore)) +
-  geom_point(shape=21,colour="black", fill="black", size = 1) + #geom_line() +
+# creat plots for scrabble 14/28 datapoints
+dataScrabbleTask14_plot <- ggplot(tableScrabbleTask14, aes(x=unit, y=nrCorrectScrabbleWords * 14)) +
+  geom_point(shape=21,colour="black", fill="black", size = 1) + 
   xlab("Time (unit)") +
   ylab("Score") +
-  ggtitle("Clear plot") +
-  scale_x_continuous(limits=c(0,25), breaks=seq(0,25, 25), minor_breaks = seq(0,150, 5)) +
-  scale_y_continuous(limits=c(0,600), breaks=seq(0,600,50)) +
+  ggtitle("Practise scrabble task (wordscore=14") +
   theme_minimal() +
   theme(axis.line = element_line(colour = "black", size = 1, linetype = "solid"))
-print(plot3)
 
-### compare to Payne's data
-time <- seq(1,600,6)  ## time steps of 6 seconds
-PayneTable <- data.frame(time = time)
-PayneTable $unit <- PayneTable $time / 6
-PayneTable $wordsPerUnitEasy <- 0.65* 0.981^(PayneTable $unit-1) 
-PayneTable $wordsPerUnitDifficult <- 0.51* 0.965^(PayneTable $unit-1)
-PayneTable $EasyWords <- cumsum(PayneTable $wordsPerUnitEasy) 
-PayneTable $DifficultWords <- cumsum(PayneTable $wordsPerUnitDifficult)
+dataScrabbleTask28_plot <- ggplot(tableScrabbleTask28, aes(x=unit, y=nrCorrectScrabbleWords * 28)) +
+  geom_point(shape=21,colour="black", fill="black", size = 1) + 
+  xlab("Time (unit)") +
+  ylab("Score") +
+  ggtitle("Practise scrabble task (wordscore=14") +
+  theme_minimal() +
+  theme(axis.line = element_line(colour = "black", size = 1, linetype = "solid"))
 
-plot4 <- plot3 + geom_point(data=PayneTable, aes(x = unit,y=EasyWords, colour="easy")) 
-plot5 <- plot4 + geom_point(data=PayneTable, aes(x = unit,y=DifficultWords, colour="hard"))
-print (plot5)
+# create model
+time <- seq(0,150,6)  ## time steps of 6 seconds
+unit <- time / 6
+
+# model: easy wordscore 14
+scrabbleModel14 <- data.frame(unit = unit)
+scrabbleModel14$wordsPerUnitEasy <- (1.55 * 14) * 0.89^(scrabbleModel14$unit)
+scrabbleModel14$EasyWords <- cumsum(scrabbleModel14$wordsPerUnitEasy)
+scrabble14Easy <- dataScrabbleTask14_plot + geom_point(data=scrabbleModel14, aes(x = unit,y=EasyWords, colour="easy"));scrabble14Easy
+
+# model: hard wordscore 14
+scrabbleModel14$wordsPerUnitHard <- (0.55 * 14) * 0.95^(scrabbleModel14$unit)
+scrabbleModel14$HardWords <- cumsum(scrabbleModel14$wordsPerUnitHard)
+scrabble14EasyHard <- scrabble14Easy + geom_point(data=scrabbleModel14, aes(x = unit,y=HardWords, colour="hard"));scrabble14EasyHard
+
+# model: easy wordscore 28
+scrabbleModel28 <- data.frame(unit = unit)
+scrabbleModel28$wordsPerUnitEasy <- (1.35 * 28) * 0.905^(scrabbleModel28$unit)
+scrabbleModel28$EasyWords <-cumsum(scrabbleModel28$wordsPerUnitEasy)
+scrabble28Easy <- dataScrabbleTask28_plot + geom_point(data=scrabbleModel28, aes(x = unit,y=EasyWords, colour="easy"))
+
+# model: hard wordscore 28
+scrabbleModel28$wordsPerUnitHard <- (0.49 * 28) * 0.95^(scrabbleModel28$unit)
+scrabbleModel28$HardWords <- cumsum(scrabbleModel28$wordsPerUnitHard)
+scrabble28EasyHard <- scrabble28Easy + geom_point(data=scrabbleModel28, aes(x = unit,y=HardWords, colour="hard"));scrabble28EasyHard
+
+# $PercentageHard <- PayneTable$DifficultWords + rev(PayneTable$EasyWords)
+
+####
+#### Combining scrabble with type taske to determine optimal scores
+####
+scrabbleModel14$PercentageScrabbleEasy <- scrabbleModel14$EasyWords + rev(typeTask14$points)
+scrabbleModel14$PercentageScrabbleHard <- scrabbleModel14$HardWords + rev(typeTask14$points)
+scrabbleModel28$PercentageScrabbleEasy <- scrabbleModel28$EasyWords + rev(typeTask28$points)
+scrabbleModel28$PercentageScrabbleHard <- scrabbleModel28$HardWords + rev(typeTask28$points)
+
+optimalScoresPlot <- ggplot(scrabbleModel14, aes(x=unit, y=PercentageScrabbleEasy)) +
+  geom_point(shape=21,colour="black", fill="black", size = 1) + 
+  xlab("Time (percentage)") +
+  ylab("Score") +
+  ggtitle("Percentage spent on scrabble vs type task") +
+  theme_minimal() +
+  theme(axis.line = element_line(colour = "black", size = 1, linetype = "solid"))
+
+optimalScoresPlot1 <- optimalScoresPlot + geom_point(data=scrabbleModel14, aes(x = unit,y=PercentageScrabbleHard, colour="hard14"))
+
+optimalScoresPlot2 <- optimalScoresPlot1 + geom_point(data=scrabbleModel28, aes(x = unit,y=PercentageScrabbleEasy, colour="easy28"))
+optimalScoresPlot3 <- optimalScoresPlot2 + geom_point(data=scrabbleModel28, aes(x = unit,y=PercentageScrabbleHard, colour="hard28"))
+
 
 
 
@@ -115,12 +148,6 @@ print (plot5)
 ####
 
 
-#data opgedeeld op basis van woordwaarde(score14Data // score28Data) en easy/hard:
-
-#easyscore14Data
-#hardscore14Data
-#easyscore28Data
-#hardscore28Data
 length(unique(score28Data$SubjectNr))
 score14DataCorrect <- score14Data[score14Data$partOfExperiment=="dualTask" & 
                                     score14Data$Eventmessage1 == "scoreGiven",]
